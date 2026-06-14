909c8bdb29e4413e25f3e30cca0c4247261c0d79
Ashley <noeminasham@gmail.com>
2026-06-13 22:06:33 -0500

    Delete AppFastSnack directory

--- AppFastSnack/src/Modelo/FastSnack.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-import Presentacion.FastSnackRecibo;
-
-public class FastSnack {
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> new FastSnackRecibo().setVisible(true));
-    }
-}
